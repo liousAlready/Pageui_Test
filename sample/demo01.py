fd7d6e5ba6856017d3ab5550fec20dd15dd0b054
limusen <m1557493385@163.com>
2021-08-03 18:00:31 +0800

    新增

---
+++ sample/demo01.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/8/3 17:56
+# @Author : Limusen
+# @File : demo01
+
+
+
+
+str = 'hello'
+str = str + 'newdream'  # ....
+
+# =====================================
+# 醒目关键注释!!!
+# =====================================
+
+"""
+This is a reST style.
+
+:param param1: this is a first param
+:param param2: this is a second param
+:returns: this is a description of what is returned
+:raises keyError: raises an exception
+
+"""
+
+"""
+This is a groups style docs.
+
+Parameters:
+  param1 - this is the first param
+  param2 - this is a second param
+
+Returns:
+  This is a description of what is returned
+
+Raises:
+  KeyError - raises an exception
+"""
+
---
+++ sample/demo2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/8/3 17:58
+# @Author : Limusen
+# @File : demo2
+
+print("hello,python")
